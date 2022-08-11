0bd4297197ae4d17a5004b5421486c6fb0e22073
Ryan Lock <lockr@Ryans-MacBook-Air.local>
2022-08-11 00:13:22 -0500

    fix handlebars engine

--- app.js
+++ app.js
@@ -5,41 +5,41 @@ This code makes a bunch of assumptions that (if violated) will definitely break 
 * The game played is Texas Hold'em (may also work for short-deck hold'em)
 */
 
-const express = require("express")
-const exphbs  = require('express-handlebars');
-const multer = require('multer')
-const fs = require('fs'); 
-const parse = require('csv-parse');
-const pokerParse = require('./parsers/parse.js');
-const stackSizes = require('./parsers/stackSizes.js');
-const handOutcomes = require('./parsers/handOutcomes.js');
-const winningHands = require('./parsers/winningHands.js');
-const ledgerTable = require('./parsers/ledgerTable.js');
-const biggestPots = require('./parsers/biggestPots.js');
-const timeToAct = require('./parsers/timeToAct.js');
-const preflopBehavior = require('./parsers/preflopBehavior.js');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const multer = require("multer");
+const fs = require("fs");
+const parse = require("csv-parse");
+const pokerParse = require("./parsers/parse.js");
+const stackSizes = require("./parsers/stackSizes.js");
+const handOutcomes = require("./parsers/handOutcomes.js");
+const winningHands = require("./parsers/winningHands.js");
+const ledgerTable = require("./parsers/ledgerTable.js");
+const biggestPots = require("./parsers/biggestPots.js");
+const timeToAct = require("./parsers/timeToAct.js");
+const preflopBehavior = require("./parsers/preflopBehavior.js");
 
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: "uploads/" });
 
-const app = express()
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-app.use(express.static(__dirname + '/public'));
+const app = express();
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.use(express.static(__dirname + "/public"));
 
-app.get('/',(req,res) => {
-  res.render('home')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.post('/upload', upload.single('logfile'), function (req, res, next) {
+app.post("/upload", upload.single("logfile"), function (req, res, next) {
   const data = [];
   fs.createReadStream(req.file.path)
-    .pipe(parse({columns: true}))
-    .on('data', (r) => {
-      data.push(r);        
+    .pipe(parse({ columns: true }))
+    .on("data", (r) => {
+      data.push(r);
     })
-    .on('end', () => {
-      let pokerGame = pokerParse.parsePokerGame(data)
-      res.render('charts', {
+    .on("end", () => {
+      let pokerGame = pokerParse.parsePokerGame(data);
+      res.render("charts", {
         stackSizes: JSON.stringify(stackSizes.parse(pokerGame)),
         handOutcomes: JSON.stringify(handOutcomes.parse(pokerGame)),
         winningHands: JSON.stringify(winningHands.parse(pokerGame)),
@@ -47,16 +47,16 @@ app.post('/upload', upload.single('logfile'), function (req, res, next) {
         biggestPots: biggestPots.parse(pokerGame),
         timeToAct: timeToAct.parse(pokerGame),
         preflopBehavior: JSON.stringify(preflopBehavior.parse(pokerGame)),
-        pokerGame: JSON.stringify(pokerGame)
-      })
+        pokerGame: JSON.stringify(pokerGame),
+      });
       fs.unlink(req.file.path, function (err) {
         if (err) throw err;
         // if no error, file has been deleted successfully
-        console.log('Logfile processed and deleted');
-      }); 
-  })
-})
+        console.log("Logfile processed and deleted");
+      });
+    });
+});
 
-let port = process.env.PORT || 3000
-app.listen(port)
-console.log("App started on port " + port)
+let port = process.env.PORT || 3000;
+app.listen(port);
+console.log("App started on port " + port);
